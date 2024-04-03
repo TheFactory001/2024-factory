da7be5b3fe7a808e1e9def3691347353c3627967
tsb050 <tsb050@mun.ca>
2024-04-02 22:01:29 -0700

    Rust object and interface

---
+++ target/debug/deps/f_structs_traits-c83b7a96836c698f.d
@@ -0,0 +1,5 @@
+/home/caviovonleib/rust-crs/exercise/f_structs_traits/target/debug/deps/f_structs_traits-c83b7a96836c698f: src/main.rs
+
+/home/caviovonleib/rust-crs/exercise/f_structs_traits/target/debug/deps/f_structs_traits-c83b7a96836c698f.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/f_structs_traits.d
@@ -0,0 +1,1 @@
+/home/caviovonleib/rust-crs/exercise/f_structs_traits/target/debug/f_structs_traits: /home/caviovonleib/rust-crs/exercise/f_structs_traits/src/main.rs
